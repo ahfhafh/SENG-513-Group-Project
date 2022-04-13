377b8735276b41a9edee94398883d807bd41dc69
Jack <ahfhafh@gmail.com>
2022-04-13 16:45:35 -0600

    most recent first

--- routers/userPageCommentsRouter.js
+++ routers/userPageCommentsRouter.js
@@ -4,7 +4,7 @@ const userPageCommentsRouter = express.Router();
 const UserModel = require('../models/user');
 
 userPageCommentsRouter.get('/userPageComments', requireLogin, (req, res) => {
-    UserModel.findOne({ _id: res.locals.user.id }).populate({ path: 'messages', populate: { path: 'course' }}).exec((err, user) => {
+    UserModel.findOne({ _id: res.locals.user.id }).populate({ path: 'messages', populate: { path: 'course' }, options: { sort: { 'createdAt': -1 } } }).exec((err, user) => {
         if (err) return handleError(err);
         res.render(('userPageComments.ejs'), { user: user, isAdmin: false });
     });
